4f95b7b1809089a9a94c801dee4c2c3b416881ef
Oliva Montaditos <olivamontaditos@gmail.com>
2026-03-03 05:13:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1946509678-fab65",
+  "appId": "1:520926939223:web:acafe90d50cf5a4d8dbb12",
+  "apiKey": "AIzaSyDCy3xlj_1Eh-CUsGYvOqKncJCa1pt5NNk",
+  "authDomain": "studio-1946509678-fab65.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "520926939223"
+};
